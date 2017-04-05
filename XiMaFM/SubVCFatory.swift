b06e36c3d5f7fa7580ddb9c3f8b18a317df9a0ca
Bing <809568764@qq.com>
2017-04-05 18:41:12 +0800

    发现页面滑动切换控件

---
+++ XiMaFM/SubVCFatory.swift
@@ -0,0 +1,13 @@
+//
+//  SubVCFatory.swift
+//  XiMaFM
+//
+//  Created by Bing on 2017/4/5.
+//  Copyright © 2017年 Bing. All rights reserved.
+//
+
+import UIKit
+
+class SubVCFatory: NSObject {
+
+}
---
+++ XiMaFM/TitlesView.swift
@@ -0,0 +1,21 @@
+//
+//  TitlesView.swift
+//  XiMaFM
+//
+//  Created by Bing on 2017/4/5.
+//  Copyright © 2017年 Bing. All rights reserved.
+//
+
+import UIKit
+
+class TitlesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
